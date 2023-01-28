047c3a564ef0a27edae767a841bb86b853b5e6f4
vestaabner <vestaabner@gmail.com>
2023-01-28 11:23:54 +0330

    feat add Entities

---
+++ DDD.Entities/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace DDD.Entities;
+
+
+
+public class CarFactory
+{
+    public static int id = 0;
+    public static Car CreateCar() {
+        return new Car(id++)
+    }
+
+}
+public class Car
+{
+    public Car(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void changeNmae(string newName)
+    {
+        if (string.IsNullOrEmpty(newName))
+            throw new Exception();
+        else
+            this.Name = newName;
+    }
+
+}
+
